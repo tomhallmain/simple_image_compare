Fixes and improvements, add image rotation

image/rotation.py
@@ -1,20 +1,28 @@
+import os
+
 import cv2 
 import numpy as np
+
+from utils.utils import move_file
+
+def rotate_image(image_path, right=False):
+	try:
+		#loading the image into a numpy array 
+		img = cv2.imread(image_path)
+		#rotating the image
+		if right:
+			rotated = np.rot90(img, k=-2)
+		else:
+			rotated = np.rot90(img, k=2)
+
+		current_extension = os.path.splitext(image_path)[-1]
+		temp_filepath = os.path.join(os.path.dirname(image_path), 'temp' + current_extension)
+		cv2.imwrite(temp_filepath, rotated)
+		move_file(temp_filepath, image_path, overwrite_existing=True)
+	except Exception as e:
+		print(f'Error in rotate image: {e}')
+
+# rotated_90_clockwise = np.rot90(img) #rotated 90 deg once 
+# rotated_180_clockwise = np.rot90(rotated_90_clockwise) 
+# rotated_270_clockwise = np.rot90(rotated_180_clockwise) 
  
-#loading the image into a numpy array 
-img = cv2.imread('<image path>') 
- 
-#rotating the image 
-rotated_90_clockwise = np.rot90(img) #rotated 90 deg once 
-rotated_180_clockwise = np.rot90(rotated_90_clockwise) 
-rotated_270_clockwise = np.rot90(rotated_180_clockwise) 
- 
-#displaying all the images in different windows(optional) 
-cv2.imshow('Original', img) 
-cv2.imshow('90 deg', rotated_90_clockwise) 
-cv2.imshow('Inverted', rotated_180_clockwise) 
-cv2.imshow('270 deg', rotated_270_clockwise) 
- 
-k = cv2.waitKey(0) 
-if (k == 27): #closes all windows if ESC is pressed 
-	cv2.destroyAllWindows() 