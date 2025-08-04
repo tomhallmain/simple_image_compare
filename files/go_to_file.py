9e49d0af55c8ef9d954a21c0354afd2c24f70735
Tom Hall <tomhall.main@gmail.com>
2025-08-04 12:48:43 -0400

    Add file picker to go to file window

--- files/go_to_file.py
+++ files/go_to_file.py
@@ -1,7 +1,8 @@
-from tkinter import Toplevel, Frame, StringVar, LEFT, W
+from tkinter import Toplevel, Frame, StringVar, LEFT, W, filedialog
 from tkinter.ttk import Entry, Button
 
 from utils.app_style import AppStyle
+from utils.config import config
 from utils.translations import I18N
 
 _ = I18N._
@@ -13,7 +14,7 @@ class GoToFile:
 
     @staticmethod
     def get_geometry():
-        width = 600
+        width = 700
         height = 100
         return f"{width}x{height}"
 
@@ -27,6 +28,7 @@ class GoToFile:
         self.frame.grid(column=0, row=0)
         self.frame.columnconfigure(0, weight=9)
         self.frame.columnconfigure(1, weight=1)
+        self.frame.columnconfigure(2, weight=1)  # column for file picker button
         self.frame.config(bg=AppStyle.BG_COLOR)
 
         self.search_text = StringVar()
@@ -36,6 +38,10 @@ class GoToFile:
         self.search_text_box.bind("<Return>", self.go_to_file)
         self.search_files_btn = None
         self.add_btn("search_files_btn", "Go To", self.go_to_file, column=1)
+        
+        # Add file picker button
+        self.file_picker_btn = None
+        self.add_btn("file_picker_btn", _("Browse..."), self.pick_file, column=2)
 
         self.master.bind("<Escape>", self.close_windows)
         self.frame.after(1, lambda: self.frame.focus_force())
@@ -49,6 +55,45 @@ class GoToFile:
         GoToFile.last_search_text = search_text
         self.app_actions.go_to_file(search_text=search_text)
         self.close_windows()
+
+    def pick_file(self, event=None):
+        """Open file picker dialog and go to selected file."""
+        # Create file type filter from config
+        file_types = []
+        if config.file_types:
+            # Group extensions by type for better organization
+            extensions = " ".join([f"*{ext}" for ext in config.file_types])
+            file_types.append((_("Supported files"), extensions))
+            # Also add individual file type groups for better organization
+            if config.image_types:
+                img_extensions = " ".join([f"*{ext}" for ext in config.image_types])
+                file_types.append((_("Image files"), img_extensions))
+            if config.enable_videos and config.video_types:
+                vid_extensions = " ".join([f"*{ext}" for ext in config.video_types])
+                file_types.append((_("Video files"), vid_extensions))
+            if config.enable_gifs:
+                file_types.append((_("GIF files"), "*.gif"))
+            if config.enable_pdfs:
+                file_types.append((_("PDF files"), "*.pdf"))
+            if config.enable_svgs:
+                file_types.append((_("SVG files"), "*.svg"))
+            if config.enable_html:
+                file_types.append((_("HTML files"), "*.html *.htm"))
+        
+        # Add "All files" option
+        file_types.append((_("All files"), "*.*"))
+        
+        selected_file = filedialog.askopenfilename(
+            title=_("Select file to go to"),
+            filetypes=file_types,
+            initialdir=self.app_actions.get_base_dir() if hasattr(self.app_actions, 'get_base_dir') else "."
+        )
+        
+        if selected_file:
+            # Set the selected file path in the search box
+            self.search_text.set(selected_file)
+            # Go to the selected file
+            self.go_to_file()
 
     def close_windows(self, event=None):
         self.master.destroy()
